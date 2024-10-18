1bb35d81fcf8235f6abff595509f00129caf0be1
Leombuam <leonardo.montenegro@uamcr.net>
2024-10-18 16:31:31 -0600

    Creación de archivos
    Se crean archivos para iniciar el proyecto.

---
+++ src/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6xHgFKCjxaxYWxCwP4VEPoCJEFFqxbe4",
+  authDomain: "proyecto-progra4-c8bed.firebaseapp.com",
+  projectId: "proyecto-progra4-c8bed",
+  storageBucket: "proyecto-progra4-c8bed.appspot.com",
+  messagingSenderId: "732748357801",
+  appId: "1:732748357801:web:db01e16d3c9a9f6dba89b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export  default app;
